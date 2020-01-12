8d1152369542bd86b099d000bfab0919bb127d54
Andre luis <andre.rodrigues98@outlook.com>
2020-01-11 21:39:21 -0300

    inserindo examples de fornecimento de dados para o layout e população de dados

---
+++ frontend/src/database/dataLanding.example.js
@@ -0,0 +1,35 @@
+export const databaseLanding = {
+    navs_links: [
+        {
+            link:"",
+            name:"",
+            type:""
+        }
+    ],
+    description_landing:"",
+    description_bussiness: "",
+    cases: [
+        {
+            link:"",
+            description:"",
+            title:"",
+            thumbnail:""
+        }
+    ],
+    contacts: [
+        {
+            name:"",
+            numbers: ["", ""],
+            email: "",
+            link: ""
+        }
+    ],
+    additional_links: [
+        {
+            name:"",
+            link:"",
+            type:""
+        }
+    ],
+    location: ""
+};
---
+++ frontend/src/database/settingsLayout.example.js
@@ -0,0 +1,11 @@
+export const settingsLayout = {
+    first_color:"",
+    second_color:"",
+    third_color:"",
+    accent_color:"",
+    dark_color:"",
+    layout: "",
+    settings_layout: {
+        
+    }
+}
